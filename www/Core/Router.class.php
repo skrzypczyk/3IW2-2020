d4f8bae8d69e3e86efd13e5a8334e0748de133bf
Skrzypczyk <yskrzypczyk@esgi.fr>
2020-11-25 18:46:01 +0100

    TP1 class router yaml correction

--- www/Core/Router.class.php
+++ www/Core/Router.class.php
@@ -1,33 +1,55 @@
 <?php
 class Router{
 
-	public $slug;
-	public $action;
-	public $controller;
-	public $routePath = "routes.yml";
-	public $listOfRoutes = [];
+	private $slug;
+	private $action;
+	private $controller;
+	private $routePath = "routes.yml";
+	private $listOfRoutes = [];
 
 
 	public function __construct($slug){
 		$this->slug = $slug;
+		$this->loadYaml();
 
+		if(empty($this->listOfRoutes[$this->slug])) $this->exception404();
+
+		$this->setController($this->listOfRoutes[$this->slug]["controller"]);
+		$this->setAction($this->listOfRoutes[$this->slug]["action"]);
 	}
 
-	public function setController(){
-		
+
+	public function loadYaml(){
+		$this->listOfRoutes = yaml_parse_file($this->routePath);
+		foreach ($this->listOfRoutes as $route) {
+			if(empty($route["controller"]) || empty($route["action"]))
+				die("Parse YAML ERROR");
+		}
 	}
 
-	public function setAction(){
 
+
+
+
+	public function setController($controller){
+		$this->controller = ucfirst($controller)."Controller";
+	}
+
+	public function setAction($action){
+		$this->action = $action."Action";
 	}
 
 
 	public function getController(){
-		
+		return $this->controller;
 	}
 
 	public function getAction(){
+		return $this->action;
+	}
 
+	public function exception404(){
+		die("Erreur 404");
 	}
 
 }
